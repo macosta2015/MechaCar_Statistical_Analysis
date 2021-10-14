ada73b1c4987898f47c45ac2bea5530410308ce0
macosta2015 <macosta2015@my.fit.edu>
2021-10-14 18:23:35 -0400

    Que onda!

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -19,8 +19,6 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 
 
 
-
-
 ####
 # Delivery 2
 # Read Suspension_Coil.csv
@@ -41,20 +39,3 @@ lot_summary<- suspension_cars %>% group_by(Manufacturing_Lot) %>% summarize(Mean
 
 
 
-####
-# Delivery3
-#t-test across all manufacturing lots against the population mean = 1500 PSI
-t.test(suspension_cars$PSI, mu=1500)
-
-
-#t-test lot1 against population mean = 1500 PSI
-lot1 <- suspension_cars %>% subset(Manufacturing_Lot=="Lot1")
-t.test(lot1$PSI, mu=1500)
-
-#t-test lot2 against population mean = 1500 PSI
-lot2 <- suspension_cars %>% subset(Manufacturing_Lot=="Lot2")
-t.test(lot2$PSI, mu=1500)
-
-#t-test lot3 against population mean = 1500 PSI
-lot3 <- suspension_cars %>% subset(Manufacturing_Lot=="Lot3")
-t.test(lot3$PSI, mu=1500)
